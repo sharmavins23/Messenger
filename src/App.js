7e6d5407d7f5e8943a8d09d046be105d92481936
Vins <sharmavins23@gmail.com>
2020-10-30 16:15:52 -0400

    🗃 added firebase basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firestore"; // Database integration
+import "firebase/auth"; // Google authentication
+
+// Hooks
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+// Initialize our Firebase project
+firebase.initializeApp({});
+
+// Global variable references
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+        </div>
+    );
 }
 
 export default App;

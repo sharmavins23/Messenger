0b8ce7811c26a933f771ca70613cc58ed4e22f10
Vins <sharmavins23@gmail.com>
2020-10-30 16:28:10 -0400

    🔥 added firebase config

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./App.css";
 import firebase from "firebase/app";
 import "firebase/firestore"; // Database integration
 import "firebase/auth"; // Google authentication
+require("dotenv").config();
 
 // Hooks
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 // Initialize our Firebase project
-firebase.initializeApp({});
+firebase.initializeApp({
+    apiKey: process.env.APIKEY,
+    authDomain: "messenger-sharmavins23.firebaseapp.com",
+    databaseURL: "https://messenger-sharmavins23.firebaseio.com",
+    projectId: "messenger-sharmavins23",
+    storageBucket: "messenger-sharmavins23.appspot.com",
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+});
 
 // Global variable references
 const auth = firebase.auth();
